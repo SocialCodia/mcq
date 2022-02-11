f7b931ffa43ee0a674cdb61ae93d68df304700a9
Social Codia <socialcodia@gmail.com>
2022-02-12 04:40:40 +0530

    Bug Fixes

--- src/components/item/mcq-item.jsx
+++ src/components/item/mcq-item.jsx
@@ -1,5 +1,5 @@
 
-const McqItem = ({ data,index }) => {
+const McqItem = ({ data, index }) => {
 
     return (
         <div className="card mt-2">
@@ -7,16 +7,16 @@ const McqItem = ({ data,index }) => {
             <div className="card-body">
                 <div className="row">
                     <div className="col-lg-6 col-xl-l col-sm-6 col-md-6 col-6">
-                        <p className={data.opOne == data.answer ? 'rightAnswer' : ''}>1) {data.opOne}</p>
+                        <p className={data.opOne === data.answer ? 'rightAnswer' : ''}>1) {data.opOne}</p>
                     </div>
                     <div className="col-lg-6 col-xl-l col-sm-6 col-md-6 col-6">
-                        <p className={data.opTwo == data.answer ? 'rightAnswer' : ''}>2) {data.opTwo}</p>
+                        <p className={data.opTwo === data.answer ? 'rightAnswer' : ''}>2) {data.opTwo}</p>
                     </div>
                     <div className="col-lg-6 col-xl-l col-sm-6 col-md-6 col-6">
-                        <p className={data.opThree == data.answer ? 'rightAnswer' : ''}>3) {data.opThree}</p>
+                        <p className={data.opThree === data.answer ? 'rightAnswer' : ''}>3) {data.opThree}</p>
                     </div>
                     <div className="col-lg-6 col-xl-l col-sm-6 col-md-6 col-6">
-                        <p className={data.opFour == data.answer ? 'rightAnswer' : ''}>4) {data.opFour}</p>
+                        <p className={data.opFour === data.answer ? 'rightAnswer' : ''}>4) {data.opFour}</p>
                     </div>
                 </div>
             </div>
--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5500/api/',
+    baseURL: 'https://api-mcq.herokuapp.com/api/',
     withCredentials: true
 });
 
