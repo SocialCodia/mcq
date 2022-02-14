5449bffe2e0d8fe48a9eb0e57494cb93103af878
Social Codia <socialcodia@gmail.com>
2022-02-14 11:18:50 +0530

    Update BASE_URL from local to online

--- src/http/index.js
+++ src/http/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-    // baseURL: 'https://api-mcq.herokuapp.com/api/',
-    baseURL: 'http://localhost:5500/api/',
+    baseURL: 'https://api-mcq.herokuapp.com/api/',
+    // baseURL: 'http://localhost:5500/api/',
     withCredentials: true
 });
 
@@ -13,7 +13,7 @@ export const getUsers = () => api.get('admin/users');
 export const addMcq = data => api.post('public/mcq', data);
 export const doLogin = data => api.post('auth/login', data);
 export const doRegister = data => api.post('auth/register', data);
-export const updateUser = data => api.patch('admin/user',data);
+export const updateUser = data => api.patch('admin/user', data);
 
 api.interceptors.response.use((response) => response.data, (error) => {
     toast.error(error.response.data.message);
